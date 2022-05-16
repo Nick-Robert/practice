12396ed8279f8d28be6fd5577d739b6b240cb522
Nick Robert <nick.robert202@gmail.com>
2022-05-16 16:31:18 -0500

    Part 6 - User Authentication
    Goes over user authentication using various Flask extensions

--- tutorial_python-flaskblog/flaskblog/__init__.py
+++ tutorial_python-flaskblog/flaskblog/__init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 # three slashes is a relative path from the current file, so the site.db will be created in the same directory as this file
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'  # function name of the route, same as the thing you pass in to the url_for() function
+login_manager.login_message_category = 'info'
 
 # need to import the routes after the app initialization to avoid circular imports
 from flaskblog import routes
