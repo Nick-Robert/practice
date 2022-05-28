367ec1be1717483566c3e08d68dbe0d345d70aa6
Nick Robert <nick.robert202@gmail.com>
2022-05-28 00:40:50 -0500

    code cleanup

--- Python/GUI/history.py
+++ Python/GUI/history.py
@@ -24,9 +24,6 @@ class HistoryButtons(History, tk.Frame):
 
         self.vsb.pack(side="right", fill="y")
         self.canvas.pack(side="left", fill="both", expand=True)
-        # self.canvas.place(rely=0.12, relheight=0.88)
-
-
 
     @property
     def lasty(self):
@@ -39,21 +36,14 @@ class HistoryButtons(History, tk.Frame):
         print(text)
     
     def _remove_all_buttons(self):
-        # print("will remove all buttons in the canvas")
         self.canvas.delete("all")
     
     def _add_all_buttons(self):
-        # print("will add all buttons according to what's in the history list")
         for index, operation in enumerate(history):
             x = 20
             y = self.lasty + x
             btn = tk.Button(self.canvas, text=operation, justify="center")
             self.canvas.create_window(20, y, anchor="nw", window=btn)
             self.buttons.append(btn)
-        # for n in range(40):
-        #     x = 20
-        #     y = self.lasty + x
-        #     btn = tk.Button(self.canvas, text=f'Button #{n}', justify="right", command=lambda te=n: self._on_click(te))
-        #     self.canvas.create_window(20, y, anchor="nw", window=btn)
         bbox = self.canvas.bbox("all")
         self.canvas.configure(scrollregion=(0, 0, bbox[2], bbox[3]))
